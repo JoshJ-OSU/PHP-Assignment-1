bc3982fd0952327a0a46bb4d726b466b7e0d0374
JoshJ-OSU <johnsjo3@onid.oregonstate.edu>
2015-05-02 13:06:05 -0700

    Get is working on multitable.  Error checking in place.  Just have to build the table now...

---
+++ src/multtable.php
@@ -0,0 +1,104 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Assignment-4 Part-1 multtable.php</title>
+	<link href="style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+$errorFlag = false;
+
+if (isset($_GET['min-multiplicand'])) {
+	//get the value
+	$min_multiplicand = $_GET['min-multiplicand'];
+	
+	//check that value type is an integer
+	if (is_numeric($min_multiplicand) == false) {
+		$errorFlag = true;
+		echo "min-multiplicand must be an integer! <br>";
+	} else {
+		settype($min_multiplicand, "int");
+	}
+} else {
+	$errorFlag = true;
+	echo "Missing parameter min-multiplicand <br>";
+}
+
+if (isset($_GET['max-multiplicand'])) {
+	//get the value
+	$max_multiplicand = $_GET['max-multiplicand'];
+		
+	//check that value type is an integer
+	if (is_numeric( $max_multiplicand) == false) {
+		$errorFlag = true;
+		echo "max-multiplicand must be an integer! <br>";
+	} else {
+		settype($max_multiplicand, "int");
+	}
+} else {
+	$errorFlag = true;
+	echo "Missing parameter max-multiplicand <br>";
+}
+
+if (isset($_GET['min-multiplier'])) {
+	//get the value
+	$min_multiplier = $_GET['min-multiplier'];
+		
+	//check that value type is an integer
+	if (is_numeric( $min_multiplier) == false) {
+		$errorFlag = true;
+		echo "min-multiplier must be an integer! <br>";
+	} else {
+		settype($min_multiplier, "int");
+	}
+} else {
+	$errorFlag = true;
+	echo "Missing parameter min-multiplier <br>";
+}
+
+if (isset($_GET['max-multiplier'])) {
+	//get the value
+	$max_multiplier = $_GET['max-multiplier'];
+	
+	//check that value type is an integer
+	if (is_numeric( $min_multiplier) == false) {
+		$errorFlag = true;
+		echo "min-multiplier must be an integer! <br>";
+	} else {
+		settype($max_multiplier, "int");
+	}
+} else {
+	$errorFlag = true;
+	echo "Missing parameter max-multiplier <br>";
+}
+
+
+// If valid parameters, check that the values are correct  
+if ($errorFlag == false) {
+	if ($min_multiplicand > $max_multiplicand) {
+		echo "min-multiplicand must be less than/equal to max-multiplicand <br>";
+		$errorFlag = true;
+	}
+	
+	if ($min_multiplier > $max_multiplier) {
+		echo "min-multiplier must be less than/equal to max-multiplier <br>";
+		$errorFlag = true;
+	}
+}
+
+if ($errorFlag == false) {
+	echo "min-multiplicand = " . $min_multiplicand . "<br>";
+	echo "max-multiplicand = " . $max_multiplicand . "<br>";
+	echo "min-multiplier = " . $min_multiplier . "<br>";
+	echo "max-multiplier = " . $max_multiplier . "<br>";
+}
+
+?>
+<p>Hello World</p>
+</body>
+</html>
